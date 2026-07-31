41ecde2f86f736a7ba1722b86d2c176501ce318e
Zachary A <zaustin004@gmail.com>
2026-07-31 10:55:21 -0600

    add transformer summarizer test call

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from frequency_summarizer import FrequencySummarizer
+from transformer_summarizer import TransformerSummarizer
 
 sample_text = """It is always a pleasure to see a familiar face wander up the 
 mountain path to the shrine. The breeze up here at the 
@@ -7,5 +8,8 @@ Though I must say, your blue tail seems to be swaying with a certain...
 focused intent. 
 You always bring the most intriguing little puzzles with you when you come to visit."""
 
-summarizer1 = FrequencySummarizer(2)
-print(summarizer1.summarize(sample_text))
+freq_sum = FrequencySummarizer(2)
+print(freq_sum.summarize(sample_text))
+
+trans_sum = TransformerSummarizer(device="cpu")
+print(trans_sum.summarize(sample_text))
